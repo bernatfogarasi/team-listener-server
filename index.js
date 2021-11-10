3bfeb33e81ed051e09571d1db09f1376fb0c97f4
Bernat Fogarasi <fogarasi.bernat@gmail.com>
2021-11-10 15:43:35 +0000

    Get port from environment

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.use(express.json());
 
 app.use("/user", authRoute);
 
-app.listen(3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
